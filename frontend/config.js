6bcc47f9a493c7ed85ff073ce87b7e2b1e5099f8
Samreen Karim <iamsamreenk@gmail.com>
2024-11-13 09:48:38 +0500

    new changes

--- frontend/config.js
+++ frontend/config.js
@@ -1,12 +1,17 @@
+import { AUTH_API, TESTS_API, QUESTIONS_API, 
+    PRACTICE_QUESTIONS_API, 
+    NOTES_API, REPORTS_API 
+} from '@env';
+
 const config = {
-    urls: {
-        AUTH_API: "http://192.168.0.104:5000/api/auth",
-        TESTS_API: "http://192.168.0.104:5000/api/tests",
-        QUESTIONS_API: "http://192.168.0.104:5000/api/questions",
-        PRACTICE_QUESTIONS_API: "http://192.168.0.104:5000/api/practiceQs",
-        NOTES_API: "http://192.168.0.104:5000/api/notes",
-        REPORTS_API: "http://192.168.0.104:5000/api/reports"
-    }
+  urls: {
+    AUTH_API,
+    TESTS_API,
+    QUESTIONS_API,
+    PRACTICE_QUESTIONS_API,
+    NOTES_API,
+    REPORTS_API,
+  }
 };
 
 export default config;
